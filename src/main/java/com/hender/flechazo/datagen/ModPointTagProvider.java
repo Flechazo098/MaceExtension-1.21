fe6eb3599004b6c37ce210ea117a8f2be1b974be
Flechazo <2558755403@qq.com>
2024-08-13 21:30:53 +0800

    further modifications

---
+++ src/main/java/com/hender/flechazo/datagen/ModPointTagProvider.java
@@ -0,0 +1,24 @@
+package com.hender.flechazo.datagen;
+
+import com.hender.flechazo.Hender;
+import net.minecraft.data.DataOutput;
+import net.minecraft.data.server.tag.TagProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.PointOfInterestTypeTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.poi.PointOfInterestType;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModPointTagProvider extends TagProvider<PointOfInterestType> {
+    public ModPointTagProvider(DataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookupFuture) {
+        super(output, RegistryKeys.POINT_OF_INTEREST_TYPE, registryLookupFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup lookup) {
+        getOrCreateTagBuilder(PointOfInterestTypeTags.ACQUIRABLE_JOB_SITE)
+                .addOptional(Identifier.of(Hender.MOD_ID, "hengmou_poi"));
+    }
+}
